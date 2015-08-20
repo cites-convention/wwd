5cb13936aeff87ae762fd5702649f7a77152e0fc
Cristian Romanescu <cristian@romanescu.ro>
2015-08-20 13:22:49 +0300

    Fix aliases file

--- drush/aliases/aliases.drushrc.php
+++ drush/aliases/aliases.drushrc.php
@@ -2,45 +2,13 @@
 /**
  * Implement this aliases in your aliases.local.php
  */
-$aliases['cms.production'] = array(
-  'uri' => 'please-fill-in-local-aliases',
-  'db-allows-remote' => TRUE,
-  'remote-host' => 'please-fill-in-local-aliases',
-  'remote-user' => 'please-fill-in-local-aliases',
+$aliases['prod'] = array(
+  'uri' => 'http://www.wildlifeday.org',
+  'remote-host' => 'www.cites.org',
   'root' => 'please-fill-in-local-aliases',
   'path-aliases' => array(
     '%files' => 'sites/default/files',
-  ),
-  'command-specific' => array(
-    'sql-sync' => array(
-      'simulate' => '1',
-      'source-dump' => '/tmp/cms-source-dump-myuser.sql',
-      'target-dump' => '/tmp/cms-target-dump-myuser.sql.gz',
-    ),
-  ),
-);
-
-$aliases['cms.staging'] = array(
-  'uri' => 'please-fill-in-local-aliases',
-  'db-allows-remote' => TRUE,
-  'remote-host' => 'please-fill-in-local-aliases',
-  'remote-user' => 'please-fill-in-local-aliases',
-  'root' => 'please-fill-in-local-aliases',
-  'path-aliases' => array(
-    '%files' => 'sites/default/files',
-  ),
-  'command-specific' => array(
-    'sql-sync' => array(
-      'simulate' => '1',
-      'source-dump' => '/tmp/cms-source-dump-myuser.sql',
-      'target-dump' => '/tmp/cms-target-dump-myuser.sql.gz',
-    ),
-  ),
-);
-
-$aliases['cms.local'] = array(
-  'uri' => 'cms.localhost',
-  'root' => '/home/user/Work/cms/docroot',
+  )
 );
 
 // Add your local aliases.
